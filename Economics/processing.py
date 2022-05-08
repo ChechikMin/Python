ed1834517b9c078b0c4a64aec2d5e5b6d962d4f5
Vladislav <u197456@mail.ru>
2022-05-08 11:08:52 +0300

    Calculate Economic process

---
+++ Economics/processing.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import copy
+
+
+def full_column_residual(income_column, dependents_column, subsistence_level=15000):
+    result = income_column - subsistence_level * dependents_column
+    return result
+
+
+def find_emission(column):
+    list_ = []
+    mean_ = np.mean(column)
+    sigma_ = np.std(column)
+    for i in range(len(column)):
+        if (np.abs(column[i]) > mean_ + 3*sigma_):
+            list_.append(i)
+    return list_
+
+
+def get_coef_annuet(month_percent, month_credit):
+    # formulae from https://journal.tinkoff.ru/guide/credit-payment/
+    coef_annuetniet = month_percent * (1 + month_percent) ** month_credit / ((1 + month_percent) ** month_credit - 1)
+    return coef_annuetniet
+
+
+def calculate_param_of_credit(sum_expceted, age, residual, month_expected=12):
+    age_threshold = 80  # this threshold adjusts max age of credit repayments, [years of person]
+    perc_credit = [0.2, 0.15, 0.12, 0.1, 0.08, 0.06]  # may be changed
+
+    # initial iteration
+    sum_credit = sum_expceted
+    month_credit = month_expected
+
+    # main loop
+    while month_credit/12 + age < age_threshold:
+
+        for percent in perc_credit:
+
+            # calculate payment_per_month
+            coef_annuet = get_coef_annuet(percent / 12, month_credit)
+            payment_per_month_credit = sum_credit * coef_annuet
+
+            # checked the condition
+            if (payment_per_month_credit < residual) and (month_credit/12 + age < age_threshold):
+                return [sum_credit, payment_per_month_credit, percent, month_credit]
+
+        # years are varied
+        month_credit += 1
+
+    print('Невозможно выдать кредит на запрошенную сумму')
+    return [0, 0, 0, 0]
